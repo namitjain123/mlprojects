d0dc067468665abdbc32199422da5d3630d32506
namit jain <57004124+namitjain123@users.noreply.github.com>
2025-11-30 23:14:41 +1100

    best model complete

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,12 +1,14 @@
 import os
 import sys
+from src.components.model_trainer import ModelTrainer
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
-
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 #
 ##Stores file paths used in the ingestion step.n ML workflow, artifacts means generated files
 @dataclass
@@ -48,6 +50,10 @@ class DataIngestion:
         
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
 
-   
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
